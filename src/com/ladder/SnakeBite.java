d91c01d3854b4ab4569a99a86e7db8ce6a1bc479
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-03-25 00:31:42 +0530

    Added: Player roll the die to get a no. b/w 1 to 6

--- src/com/ladder/SnakeBite.java
+++ src/com/ladder/SnakeBite.java
@@ -5,5 +5,8 @@ public class SnakeBite {
 		final int start = 0;
 		System.out.println("Single Player Start possition 0");
 		System.out.println("Welcome to the Snake Ladder game");
+		//to get random value
+		int dieValue = (int) ((Math.random() * 100) % 6) + 1;
+		System.out.println(dieValue);
 	}
 }
