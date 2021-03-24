c0c1c544045b204a61ae5008a803b482efa645dd
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-03-25 00:22:57 +0530

    Added: Player Start with Possition 0

--- src/com/ladder/SnakeBite.java
+++ src/com/ladder/SnakeBite.java
@@ -1,6 +1,9 @@
 package com.ladder;
+
 public class SnakeBite {
 	public static void main(String[] args) {
+		final int start = 0;
+		System.out.println("Single Player Start possition 0");
 		System.out.println("Welcome to the Snake Ladder game");
 	}
 }
